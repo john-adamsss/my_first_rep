022000d495b46b8333cfe885a98bc3a6625ddc2e
John <jjjohnadamsss@gmail.com>
2020-04-25 20:14:01 +0300

    CFSA3 done

---
+++ amstrong.py
@@ -0,0 +1,17 @@
+a = input("Enter a number please \n")
+liste = list(a)
+if  a.isdigit() == True :
+    c = 0
+    for b in liste:
+        b = int(b)
+        c += b**len(liste)
+    a = int(a)
+    if c == a:
+        print ("{} is an amstrong number.".format(a))
+    else: 
+        print ("{} is not an amstrong number.".format(a))
+elif "-" in liste:
+    print("Please enter a positive number")
+elif "." in liste or "," in liste:3,2
+    print("Please enter an integer number")
+else : print("Do not use any entries other than numeric values")
